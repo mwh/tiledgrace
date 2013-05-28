ac99d63d1187344f12ecbbdd35e90bd570dec508
Michael Homer <mwh@ecs.vuw.ac.nz>
2013-05-28 14:36:38 +1200

    Recompile text in background to rebuild tiles

--- main.js
+++ main.js
@@ -611,6 +611,7 @@ function shrink() {
         setTimeout(function() {
             ctr.style.visibility = 'visible';
             codearea.style.visibility = 'hidden';
+            document.getElementById('indicator').style.background = 'green';
         }, 1100);
     }, 700);
 }
@@ -622,24 +623,8 @@ function grow() {
         minigrace.compile(editor.getValue() + chunkLine);
         minigrace.mode = "js";
         if (minigrace.compileError) {
-            var lines = document.getElementById('stderr_txt').value.split("\n");
-            var errmsg = "";
-            for (var i=0; i<lines.length; i++) {
-                if (lines[i].substring(0, 10) != 'minigrace:') {
-                    errmsg += lines[i].substring(11) + "\n";
-                    break;
-                }
-            }
-            var bits = errmsg.split(':');
-            editor.moveCursorTo(bits[0] - 1, bits[1] - 1);
-            editor.getSelection().clearSelection();
-            editor.getSession().setAnnotations([{
-                row: bits[0] - 1,
-                column: bits[1] - 1,
-                text: errmsg.substring(bits[0].length + bits[1].length + 3),
-                type: "error"
-            }]);
-            if (confirm("This code did not compile: " + errmsg + "\nDo you want to revert to the previous version?")) {
+            showErrorInEditor(document.getElementById('stderr_txt').value);
+            if (confirm("This code did not compile: " + errmsg + "\nDo you want to revert to the previous version that did?")) {
                 editor.setValue(document.getElementById('gracecode').value, -1);
                 editor.getSession().clearAnnotations();
                 return;
@@ -647,42 +632,9 @@ function grow() {
             return;
         }
         editor.getSession().clearAnnotations();
-        codearea.classList.add("no-transition");
-        codearea.classList.remove('shrink');
-        loadJSON(minigrace.generated_output);
-        checkpointSave();
-        var leftEdge = (document.getElementsByClassName('ace_gutter')[0].offsetWidth + 4) + 'px';
-        var runningTop = 0;
-        if (document.getElementById('dialect').value)
-            runningTop = 19;
-        var starts = [];
-        for (var i=0; i<codearea.children.length; i++) {
-            var child = codearea.children[i];
-            if (child.prev != false)
-                continue;
-            starts.push(child);
-            continue;
-        }
-        codearea.classList.add('shrink');
-        for (var i=0; i<starts.length; i++) {
-            starts[i].oldTop = starts[i].style.top;
-            starts[i].oldLeft = starts[i].style.left;
-            starts[i].style.left = leftEdge;
-            starts[i].style.top = runningTop + 'px';
-            runningTop += +starts[i].offsetHeight;
-            var child = starts[i].next;
-            while (child) {
-                child.oldTop = child.style.top;
-                child.oldLeft = child.style.left;
-                child.style.left = leftEdge;
-                child.style.top = runningTop + 'px';
-                runningTop += +child.offsetHeight;
-                child = child.next;
-            }
-            runningTop += 19;
-        }
-        codearea.classList.remove("no-transition");
+        rebuildTilesInBackground(minigrace.generated_output);
     }
+    document.getElementById('indicator').style.background = 'white';
     ctr.style.visibility = 'hidden';
     codearea.style.visibility = 'visible';
     setTimeout(function() {
@@ -708,6 +660,60 @@ function toggleShrink() {
         grow();
     else
         shrink();
+}
+function rebuildTilesInBackground(jsonStr) {
+    codearea.classList.add("no-transition");
+    codearea.classList.remove('shrink');
+    loadJSON(jsonStr);
+    checkpointSave();
+    var leftEdge = (document.getElementsByClassName('ace_gutter')[0].offsetWidth + 4) + 'px';
+    var runningTop = 0;
+    if (document.getElementById('dialect').value)
+        runningTop = 19;
+    var starts = [];
+    for (var i=0; i<codearea.children.length; i++) {
+        var child = codearea.children[i];
+        if (child.prev != false)
+            continue;
+        starts.push(child);
+        continue;
+    }
+    codearea.classList.add('shrink');
+    for (var i=0; i<starts.length; i++) {
+        starts[i].oldTop = starts[i].style.top;
+        starts[i].oldLeft = starts[i].style.left;
+        starts[i].style.left = leftEdge;
+        starts[i].style.top = runningTop + 'px';
+        runningTop += +starts[i].offsetHeight;
+        var child = starts[i].next;
+        while (child) {
+            child.oldTop = child.style.top;
+            child.oldLeft = child.style.left;
+            child.style.left = leftEdge;
+            child.style.top = runningTop + 'px';
+            runningTop += +child.offsetHeight;
+            child = child.next;
+        }
+        runningTop += 19;
+    }
+    codearea.classList.remove("no-transition");
+}
+function showErrorInEditor(errstr) {
+    var lines = errstr.split("\n");
+    var errmsg = "";
+    for (var i=0; i<lines.length; i++) {
+        if (lines[i].substring(0, 10) != 'minigrace:') {
+            errmsg += lines[i].substring(11) + "\n";
+            break;
+        }
+    }
+    var bits = errmsg.split(':');
+    editor.getSession().setAnnotations([{
+        row: bits[0] - 1,
+        column: bits[1] - 1,
+        text: errmsg.substring(bits[0].length + bits[1].length + 3),
+        type: "error"
+    }]);
 }
 function findVarAssignsInScope(varname, el, accum) {
     var e = el;
