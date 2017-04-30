85e5d7853bb2541018de71ac7e1091a845d9d165
Cameron <cambo_the_rambo@hotmail.com>
2017-05-01 11:50:35 +1200

    Using Port 5000

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ app.get('/index.html', function (req, res) {
    res.sendFile( __dirname + "/" + "index.html" );
 })
 
-var server = app.listen(8081, function () {
-   var host = server.address().address
-   var port = server.address().port
-   
-   console.log("Example app listening at http://%s:%s", host, port)
+app.listen(process.env.PORT || 5000, function() {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
-})
+// var server = app.listen(8081, function () {
+//    var host = server.address().address
+//    var port = server.address().port
+   
+//    console.log("Example app listening at http://%s:%s", host, port)
+
+// })
